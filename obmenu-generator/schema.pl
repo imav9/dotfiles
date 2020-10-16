dbce7e8093751036f5ee60e0b8d3f7939901ad5a
imav9 <72993287+imav9@users.noreply.github.com>
2020-10-16 19:38:59 +0200

    Add files via upload

---
+++ obmenu-generator/schema.pl
@@ -0,0 +1,81 @@
+
+#!/usr/bin/perl
+
+# obmenu-generator - schema file
+
+=for comment
+
+    item:      add an item inside the menu               {item => ["command", "label", "icon"]},
+    cat:       add a category inside the menu             {cat => ["name", "label", "icon"]},
+    sep:       horizontal line separator                  {sep => undef}, {sep => "label"},
+    pipe:      a pipe menu entry                         {pipe => ["command", "label", "icon"]},
+    file:      include the content of an XML file        {file => "/path/to/file.xml"},
+    raw:       any XML data supported by Openbox          {raw => q(...)},
+    beg:       begin of a category                        {beg => ["name", "icon"]},
+    end:       end of a category                          {end => undef},
+    obgenmenu: generic menu settings                {obgenmenu => ["label", "icon"]},
+    exit:      default "Exit" action                     {exit => ["label", "icon"]},
+
+=cut
+
+# NOTE:
+#    * Keys and values are case sensitive. Keep all keys lowercase.
+#    * ICON can be a either a direct path to an icon or a valid icon name
+#    * Category names are case insensitive. (X-XFCE and x_xfce are equivalent)
+
+require "$ENV{HOME}/.config/obmenu-generator/config.pl";
+
+## Text editor
+my $editor = $CONFIG->{editor};
+
+our $SCHEMA = [
+
+    #          COMMAND                 LABEL              ICON
+    {item => ['thunar',       'File Manager', 'system-file-manager']},
+    {item => ['kitty',            'Terminal',     'utilities-terminal']},
+    {item => ['brave', 'Web Browser',  'web-browser']},
+    {item => ['gmrun',            'Run command',  'system-run']},
+
+    {sep => 'Categories'},
+
+    #          NAME            LABEL                ICON
+    {cat => ['utility',     'Accesories', 'applications-utilities']},
+    {cat => ['development', 'Development', 'applications-development']},
+    {cat => ['education',   'Education',   'applications-science']},
+    {cat => ['game',        'Games',       'applications-games']},
+
+  
+    {cat => ['graphics',    'Graphics',    'applications-graphics']},
+    {cat => ['audiovideo',  'Multimedia',  'applications-multimedia']},
+    {cat => ['network',     'Network',     'applications-internet']},
+    {cat => ['office',      'Office',      'applications-office']},
+    {cat => ['other',       'Other',       'applications-other']},
+    {cat => ['settings',    'Settings',    'applications-accessories']},
+    {cat => ['system',      'System',      'applications-system']},
+
+    #             LABEL          ICON
+    #{beg => ['My category',  'cat-icon']},
+    #          ... some items ...
+    #{end => undef},
+
+    #            COMMAND     LABEL        ICON
+    #{pipe => ['obbrowser', 'Disk', 'drive-harddisk']},
+
+    ## Generic advanced settings
+    #{sep       => undef},
+    #{obgenmenu => ['Openbox Settings', 'applications-engineering']},
+    #{sep       => undef},
+
+{sep => 'Exit'},
+
+    ## The xscreensaver lock command
+    {item => ['slock', 'Lock', 'system-lock-screen']},
+
+    ## This option uses the default Openbox's "Exit" action
+    {exit => ['Exit', 'application-exit']},
+
+    {exit => ['Shutdown', 'poweroff']}
+
+    ## This uses the 'oblogout' menu
+    # {item => ['oblogout', 'Exit', 'application-exit']},
+]
